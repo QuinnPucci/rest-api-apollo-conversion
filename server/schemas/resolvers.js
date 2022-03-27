a2cd0e13c1e071c773c0398a8dd13f9266451b5f
QuinnPooch <pucciquinn@gmail.com>
2022-03-27 12:11:16 -0600

    wrote backend queries and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,3 +6,44 @@ const { AuthenticationError } = require('apollo-server-express');
 
 // Import the Authentication Token generator function
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async () => {
+            return User.find();
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            // If user is not found
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            // We got isCorrectPassword() from the User model
+            const correctPw = await user.isCorrectPassword(password);
+            // If password doesnt match
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, { input }, { user }) => {
+            if (user) {
+              const userData = User.findByIdAndUpdate(
+                user._id,
+                { $push: { savedBooks: input } },
+                { new: true, runValidators: true }
+              );
+              return userData;
+            }
+            throw new AuthenticationError("You need to be logged in to save a book");
+        },
+    }
+}
