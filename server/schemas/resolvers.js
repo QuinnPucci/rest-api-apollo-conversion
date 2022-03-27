b183854b784a68c67c303ab82a84447ed04d7162
QuinnPooch <pucciquinn@gmail.com>
2022-03-27 12:09:18 -0600

    adjusted app.js on front end and server.js to include graphQL and Apollo, started type defs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,8 @@
+// Import Mongoose models
+const { User } = require('../models');
+
+// For handling login autheticatin errors
+const { AuthenticationError } = require('apollo-server-express');
+
+// Import the Authentication Token generator function
+const { signToken } = require('../utils/auth');
